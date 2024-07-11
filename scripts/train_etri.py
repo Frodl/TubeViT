f3d1e0dacd1a2efd7dd7cca622e6491d46e45c83
frodl <flohue3@hotmail.com>
2024-07-11 13:29:39 +0200

    add changes from other branch

--- scripts/train_etri.py
+++ scripts/train_etri.py
@@ -28,7 +28,7 @@ from lightning.pytorch.callbacks import ModelCheckpoint
 
 @click.option("-nc", "--num-classes", type=int, default=55, help="num of classes of dataset.")
 @click.option("-b", "--batch-size", type=int, default=8, help="batch size.")
-@click.option("-m", "--max_number_frames", type=int, default=400, help="frame per clip.")
+@click.option("-m", "--max_number_frames", type=int, default=150, help="frame per clip.")
 @click.option("-v", "--video-size", type=click.Tuple([int, int]), default=(512, 424), help="Height and width of image.")
 @click.option("-s", "--presample", type=int, default=4, help="pre sample frames from video")
 @click.option("--strides", help="sampeling for tubes")
@@ -52,6 +52,7 @@ def main(
     preview_video,
     max_number_frames,
 ):
+    remove_background = False
     pl.seed_everything(seed)
     # wandb.init(project="sparse_tubes", 
     #            name="TubeViT_{batch_size}_{max_number_frames}_{sample}_{max_epochs}_{num_workers}",
@@ -65,19 +66,19 @@ def main(
     
 
     train_transform = T.Compose(
-        [   min_max_normalization(scale_up=True),
-            ConvertToUint8(),
-            ToTensorVideo(),  # C, T, H, W
-            repeat_color_channel(), 
+        [   #min_max_normalization(scale_up=False),
+            ConvertToFloat32(),
+            #ToTensorVideo(),  # C, T, H, W
+            #repeat_color_channel(), 
             sample_frames(nth=presample)
         ]
     )
 
     test_transform = T.Compose(
-        [   min_max_normalization(scale_up=True),
-            ConvertToUint8(), # if we first scale to [0,1] and then use uint8, we will get all zeros
-            ToTensorVideo(),  # C, T, H, W
-            repeat_color_channel(), 
+        [   #min_max_normalization(scale_up=False),
+            ConvertToFloat32(), # if we first scale to [0,1] and then use uint8, we will get all zeros
+            #ToTensorVideo(),  # C, T, H, W
+            #repeat_color_channel(), 
             sample_frames(nth=presample)
         ]
     )
@@ -91,7 +92,7 @@ def main(
     train_set =  ETRIDataset(
         root_dir=dataset_root,
         mode = "train",
-        remove_background=True,
+        remove_background=remove_background,
         transform=train_transform,
         single_camera=True,
         elders_only=True,
@@ -111,7 +112,7 @@ def main(
     val_set =  ETRIDataset(
         root_dir=dataset_root,
         mode = "val",
-        remove_background=True,
+        remove_background=remove_background,
         transform=test_transform,
         single_camera=True,
         elders_only=True,
@@ -143,15 +144,16 @@ def main(
     x, y = next(iter(train_dataloader))
     print(x.shape)
 
-
+    hidden_dim=768#//4
+    mlp_dim=3072#//4
 
     model = TubeViTLightningModule(
         num_classes=num_classes,
         video_shape=x.shape[1:],
-        num_layers=4,
-        num_heads=4,
-        hidden_dim=768//4,
-        mlp_dim=3072//4,
+        num_layers=12,
+        num_heads=12,
+        hidden_dim=hidden_dim,
+        mlp_dim=mlp_dim,
         lr=1e-4,
         weight_decay=0.001,
         weight_path=os.path.join("..","saved_weights","nc200_3color_channels.pt"),
@@ -159,14 +161,14 @@ def main(
         strides=strides,
     )
 
-    #get dictionary of arguments
+    #get dictionary of arguments for name of wandb run
     frame = inspect.currentframe()
     args, _, _, values = inspect.getargvalues(frame)
     arguments_dict = {arg: values[arg] for arg in args}
 
     wandb_logger = WandbLogger(
         project="sparse_tubes", 
-        name="TubeViT_{batch_size}_{max_number_frames}_{presample}_{max_epochs}_{num_workers}",
+        name=f"TubeViT_{batch_size}_{max_number_frames}_{presample}_{max_epochs}_{num_workers}_{hidden_dim}_{mlp_dim}_False_False",
         config=arguments_dict,)
 
     callbacks = [
